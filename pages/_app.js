6fc049deb837fb3e89611a408ce9957b00e342c4
phuong1331phuong <85828804+phuong1331phuong@users.noreply.github.com>
2021-11-27 15:27:24 +0700

    add all

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+// 1. Import the extendTheme function
+// import { extendTheme } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react"
+import Layout from '../component/layout/main'
+import theme from '../component/layout/theme'
+// 2. Extend the theme to include custom colors, fonts, etc
+// const colors = {
+//   brand: {
+//     900: "#1a365d",
+//     800: "#153e75",
+//     700: "#2a69ac",
+//   },
+// }
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// const theme = extendTheme({ colors })
+
+// 3. Pass the `theme` prop to the `ChakraProvider`
+function App({ Component, pageProps , router }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout router={router}>
+      <Component {...pageProps} key={router.route}/>
+      </Layout>
+    </ChakraProvider>
+  )
 }
 
-export default MyApp
+export default App
