16a54fd0d81839da3da9d703d40dca9e742f97e3
phuong1331phuong <85828804+phuong1331phuong@users.noreply.github.com>
2021-11-27 16:32:09 +0700

    commit again

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import { Text, Box, Container } from "@chakra-ui/react"
 
-
 const HomePage = () => {
   return(
     <Container>
       <Box borderRadius="lg" bg="red" p={3} mb={6} align='center'>
-        <Text>Hello, World!</Text>
+        <Text style={{fontFamily: 'Comforter, cursive',}}>Hello, World!</Text>
       </Box>
     </Container>
   )
